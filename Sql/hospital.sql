d1ed5c95baddaa4e33e31a7fa1b3e494c96d0853
bryan01010 <bryanbarria45@gmail.com>
2026-09-15 10:18:04 -0300

    Update hospital.sql

--- Sql/hospital.sql
+++ Sql/hospital.sql
@@ -93,9 +93,9 @@ CREATE TABLE qr (
 );
 
 CREATE TABLE encuesta (
-  id_encuesta  int NOT NULL AUTO_INCREMENT,
+  id_encuesta int NOT NULL AUTO_INCREMENT,
   id_documento int,
-  fecha        datetime DEFAULT current_timestamp(),
+  fecha datetime DEFAULT current_timestamp(),
   PRIMARY KEY (id_encuesta),
   FOREIGN KEY (id_documento) REFERENCES documento (id_documento)
 );
@@ -103,10 +103,19 @@ CREATE TABLE encuesta (
 CREATE TABLE pregunta (
   id_pregunta int NOT NULL AUTO_INCREMENT,
   id_encuesta int NOT NULL,
-  texto       varchar(255),
-  respuesta   varchar(255),
+  texto varchar(255),
   PRIMARY KEY (id_pregunta),
   FOREIGN KEY (id_encuesta) REFERENCES encuesta (id_encuesta)
+);
+
+CREATE TABLE respuesta (
+  id_respuesta int NOT NULL AUTO_INCREMENT,
+  id_pregunta int NOT NULL,
+  id_paciente int NOT NULL,
+  respuesta varchar(255),
+  PRIMARY KEY (id_respuesta),
+  FOREIGN KEY (id_pregunta) REFERENCES pregunta (id_pregunta),
+  FOREIGN KEY (id_paciente) REFERENCES paciente (id_paciente)
 );
 
 CREATE TABLE encuesta_paciente_entra (
